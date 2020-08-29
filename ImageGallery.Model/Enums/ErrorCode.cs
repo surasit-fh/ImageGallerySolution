7c6c2d6515cd6dda29bf72baac560495383b1d5a
surasit.fhaisatja <topsecret.top@hotmail.com>
2020-08-29 14:10:50 +0700

    Add ImageGallery.Model

---
+++ ImageGallery.Model/Enums/ErrorCode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageGallery.Model.Enums
+{
+    public enum ErrorCode : int
+    {
+        Success = 20000,
+        BadRequest = 40000,
+        Unauthorized = 40100,
+        Forbidden = 40300,
+        NotFound = 40400,
+        InternalServerError = 50000
+    }
+}
